9ec850795d489d3be77fb4d8699b2af0cdaa1947
prernanain27 <prernanain27@gmail.com>
2017-02-13 01:58:39 -0800

    Second Commit

--- app/src/main/java/taco/android/csulb/edu/tacopronto/MainActivity.java
+++ app/src/main/java/taco/android/csulb/edu/tacopronto/MainActivity.java
@@ -2,12 +2,34 @@ package taco.android.csulb.edu.tacopronto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends  AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] fillings = getResources().getStringArray(R.array.fillings);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,fillings);
+        GridView grid = (GridView)findViewById(R.id.gridView);
+
+        grid.setAdapter(adapter);
+        for (int i=0; i<adapter.getCount();i++){
+            grid.setItemChecked(i,true);
+        }
+
+        String[] bevrages = getResources().getStringArray(R.array.bevrages);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,bevrages);
+        GridView grid2 = (GridView)findViewById(R.id.gridView2);
+
+        grid2.setAdapter(adapter2);
+        for (int i=0; i<adapter2.getCount();i++){
+            grid2.setItemChecked(i,true);
+        }
     }
 }
